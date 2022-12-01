b53b8c9cd01a5681d026fbbeae9a747f0818cd62
legion <abdukarimuz@mail.ru>
2022-12-01 23:04:49 +0500

    Initial commit

---
+++ src/main/java/uz/abdukarim/Likopchik/LikopchikApplication.java
@@ -0,0 +1,13 @@
+package uz.abdukarim.Likopchik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikopchikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikopchikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/abdukarim/Likopchik/LikopchikApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.abdukarim.Likopchik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikopchikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
